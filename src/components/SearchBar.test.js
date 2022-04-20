05a43baa73c65bb622cd83a3a4fe90932fb15cc4
FedericoBartoli <federico.bartoli@exelab.com>
2022-04-20 17:44:28 +0200

    Improve test in searchbar component

--- src/components/SearchBar.test.js
+++ src/components/SearchBar.test.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import SearchBar from './SearchBar';
-import ReactTags from 'react-tag-autocomplete';
 
 import { render, fireEvent } from '@testing-library/react';
 
@@ -8,5 +7,17 @@ describe('SearchBar component', () => {
   test('UI SearchBar ', () => {
     const { container } = render(<SearchBar />);
     expect(container).toMatchSnapshot();
+  });
+  test('should contain heading text', () => {
+    render(<SearchBar />);
+    expect(screen.getByText('Insert a comma after each keyword')).toBeVisible();
+  });
+
+  test('should call handleFilter function when filter button is pressed', () => {
+    const handleFilter = jest.fn();
+    render(<SearchBar handleFilter={handleFilter} />);
+    const filterButton = screen.getByText('FILTER');
+    fireEvent.click(filterButton);
+    expect(handleFilter).toHaveBeenCalledTimes(1);
   });
 });
